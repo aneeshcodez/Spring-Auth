93cfd83dced5fcacd400ff1d6fd88126ad655648
Aneeshsshankar <aneeshsshankar@gmail.com>
2026-03-01 23:30:29 +0530

    Progress

--- demo/src/main/java/com/example/demo/Service/JwtService.java
+++ demo/src/main/java/com/example/demo/Service/JwtService.java
@@ -1,5 +1,22 @@
 package com.example.demo.Service;
 
+
+import io.jsonwebtoken.Claims;
+
+import java.util.function.Function;
+
 public class JwtService {
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public  <T> T extractClaim(String token, Function<Claims,T> claimsResolver) {
+        final Claims claims = extractAllClaim(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaim(String token) {
+    }
 
 }
